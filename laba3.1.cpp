f9dddf2f8aa7c0da408060444007f9828961a5ec
AlexDudnik <alexdudnik2005@yandex.ru>
2023-11-11 00:47:01 +0300

    Create laba3.1.cpp

---
+++ laba3.1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+#include <stdlib.h>
+int main()
+{
+	double m[10];
+	srand(time(NULL));
+	for (int i = 0; i < 10; ++i) {
+		m[i] = rand() % 99 + 1;
+		cout << m[i] << " ";
+	}
+	cout << endl << "punkt nomer: ";
+	int p,a;
+	cin >> p;
+	switch (p) {
+	case 1:
+		for (int i = 0; i < 10; ++i) {
+			m[i] = m[i] * 2;
+			cout << m[i] << " ";
+		}
+		break;
+
+	case 2:
+		cout << "Vvedite chislo na kotoroe umenshite: ";
+		cin >> a;
+		for (int i = 0; i < 10; ++i) {
+			m[i] = m[i] - a;
+			cout << m[i] << " ";
+		}
+		break;
+	case 3:
+		int k = m[0];
+		for (int i = 0; i < 10; ++i) {
+			m[i] = m[i] / k;
+			cout << m[i]<<" ";
+		}
+		break;
+	}
+}
